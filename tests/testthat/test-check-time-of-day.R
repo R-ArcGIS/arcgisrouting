98671008d95208074d431ae185f6ce158aa6b152
Josiah Parry <josiah.parry@gmail.com>
2024-09-02 10:10:46 -0700

    slow but steady progress

--- tests/testthat/test-check-time-of-day.R
+++ tests/testthat/test-check-time-of-day.R
@@ -1,7 +1,19 @@
 test_that("time of day checks", {
-  check_time_of_day(NULL)
-  check_time_of_day(Sys.time())
-  check_time_of_day(rep(Sys.time(), 2))
-  check_time_of_day("3245")
-  check_time_of_day(list())
+  expect_no_error(check_time_of_day(NULL))
+  expect_no_error(check_time_of_day(Sys.time()))
+
+  # these are invalid
+  expect_error(check_time_of_day(rep(Sys.time(), 2)))
+  expect_error(check_time_of_day("3245"))
+  expect_error(check_time_of_day(list()))
+})
+
+test_that("validate u turn arguments", {
+  expect_identical(validate_u_turns("allow_backtrack"), "esriNFSBAllowBacktrack")
+  expect_identical(validate_u_turns("deadend_intersection"), "esriNFSBAtDeadEndsAndIntersections")
+  expect_identical(validate_u_turns("deadend"), "esriNFSBAtDeadEndsOnly")
+  expect_identical(validate_u_turns("no_backtrack"), "esriNFSBNoBacktrack")
+  expect_identical(validate_u_turns(NULL), NULL)
+  expect_error(validate_u_turns(1))
+  expect_error(validate_u_turns(c("deadend", "allow_backtrack")))
 })
--- travel-cost-matrix.R
+++ travel-cost-matrix.R
@@ -1,67 +1,72 @@
 # Synchronous service
-# 
+#
 # https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#origins
-# 
+#
 # Required parameters
 #   origins - points comma separated or feature collection
 #   destinations - same as above
-#   token - 
+#   token -
 #   f - json
 # Optional parameters
 #   travelMode - m
 #   defaultCutoff - numeric
-#   defaultTargetDestinationCount - 
+#   defaultTargetDestinationCount -
 #   outputType - I think this can be omitted
-#   timeOfDay - default uses the local time 
+#   timeOfDay - default uses the local time
 #   timeOfDayIsUTC - automatically set this is time of day is provided
 #   useHierarchy - easy
-#   restrictUTurns - 
-#   impedanceAttributeName
-#   accumulateAttributeNames
-#   restrictionAttributeNames
-#   attributeParameterValues
-#   barriers
-#   polylineBarriers
-#   polygonBarriers
+#   restrictUTurns -
+#   impedanceAttributeName -
+#   accumulateAttributeNames - 
+#   restrictionAttributeNames - 
+#   attributeParameterValues - 
+#   barriers - needs to be treated as points, max 250, can have a additional fields
+#   polylineBarriers - lines, max 500
+#   polygonBarriers - polygon, 
 #   returnOrigins
 #   returnDestinations
 #   returnBarriers
 
 
-#' @param travel_mode default `NULL`. A scalar character of the travel mode's ID. 
+#' @param travel_mode default `NULL`. A scalar character of the travel mode's ID.
 #' @param cutoff default `NULL`. Determines the maximum distance or time to stop searching.
 #' @param n_target_destinations a numeric scalar. By default all origins and destinations are matched.
-#' @param time_of_day default `NULL`. A character or `POSIXlt` scalar representing the time of day when the cost is to be calculated. If a character vector is provided, it as processed with `as.POSIXlt()` and uses the default time zone. If the time zone is `UTC` the parameter [`timeOfDayIsUTC`](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#timeofdayisutc) is set to `true`. 
+#' @param time_of_day default `NULL`. A character or `POSIXlt` scalar representing the time of day when the cost is to be calculated. If a character vector is provided, it as processed with `as.POSIXlt()` and uses the default time zone. If the time zone is `UTC` the parameter [`timeOfDayIsUTC`](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#timeofdayisutc) is set to `true`.
 #' @param use_hierarchy default `NULL`. Prefer higher-order streets (e.g. freeways). Supressed by `travel_mode`. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#usehierarchy))
-#' @param u_turns default `"allow_backtrack"`. Must be one of `"allow_backtrack"`, `"deadend_intersection"`, `"deadend"`, or `"no_backtrack"`. Determines the conditions when a U-turn is permitted. 
+#' @param u_turns default `"allow_backtrack"`. Must be one of `"allow_backtrack"`, `"deadend_intersection"`, `"deadend"`, or `"no_backtrack"`. Determines the conditions when a U-turn is permitted.
+#' @param impedance default `"travel_time"`. Determines how travel distance is measured. Must be one of `"travel_time"`, `"minutes"`, `"truck_travel_time"`, `"truck_minutes"`, `"walk_time"`, `"miles"`, `"kilometers"`. Suppressed by `travel_mode`. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#impedanceattributename))
+#' @param accumulate_impedance default `NULL`. Calculate additional impedance metrics. These will be reported, but not used in calculating the best route. Has same values as `impedance`. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#accumulateattributenames))
 #' 
 #' ## Argument Details
-#' 
+#'
 #' ### `travel_mode`
-#' 
+#'
 #' When the `travel_mode` parameter is set, you are choosing a travel mode configured in your organization, and the service automatically overrides the values of other parameters with values that model the chosen travel mode. The following parameters are overridden: `impedance_attribute_name` , `attribute_parameter_values` , `restrict_uturns` , `use_hierarchy` , `restriction_attribute_names` , and `directions_time_attribute_name`. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#travelmode))
-#' 
+#'
 #' ### `cutoff`
-#' 
+#'
 #' The travel time or travel distance value at which to stop searching for destinations from a given origin. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#defaultcutoff))
-#' 
+#'
 #' ### `n_target_destinations`
-#' 
+#'
 #' The maximum number of destinations to find per origin. If a value for this parameter is not specified, the output matrix includes travel costs from each origin to every destination. ([Reference](https://developers.arcgis.com/rest/routing/origin-destination-cost-matrix-synchronous-service/#defaulttargetdestinationcount))
-
-
 #' @export
 travel_cost_matrix <- function(
-  origin,
-  destinations,
-  travel_mode = NULL,
-  cutoff = NULL, # defaultCutoff
-  time_of_day = NULL,
-  use_hierarchy = NULL,
-  u_turns = NULL, 
-
-) {
-  # travel_mode, if provided needs to be turned into JSON from 
+    origin,
+    destinations,
+    travel_mode = NULL,
+    cutoff = NULL, # defaultCutoff
+    time_of_day = NULL,
+    use_hierarchy = NULL,
+    u_turns = NULL,
+    impedance = NULL,
+    acumulate_impedence = NULL,
+    restrictions = NULL,
+    point_barriers = NULL,
+    line_barriers = NULL,
+    polygon_barriers = NULL
+  ) {
+  # travel_mode, if provided needs to be turned into JSON from
   # retrieve_travel_modes() stored in the attributeParameterValues column
 
   # TODO default_cutoff: we can set Cutoff_[Impedance] to provide a per feature
@@ -72,15 +77,29 @@ travel_cost_matrix <- function(
 
   # set time of day is UTC parameter
   if (!is.null(time_of_day)) {
-    time_of_day_is_utc <- is_utc(time_of_day) 
+    time_of_day_is_utc <- is_utc(time_of_day)
   } else {
     time_of_day_is_utc <- FALSE
   }
 
   check_bool(use_hierarchy, allow_null = TRUE)
 
-  # validate the u-turnsargument
+  # validate the u-turns argument
   u_turns <- validate_u_turns(u_turns)
+  
+  # the impedance value
+  impedance <- validate_impedance_value(impedance)
+
+  # allow for multiple
+  accumulate_impedence <- validate_impedance_value(accumulate_impedence, multiple = TRUE)
+  
+  # if this is not null, we collapse to a comma separated list
+  if (!is.null(accumulate_impedence)) {
+    accumulate_impedence <- paste(accumulate_impedence, collapse = ",")
+  }
+
+  restrictions <- validate_restrictions(restrictions)
+  # TODO
 }
 
 
@@ -89,9 +108,9 @@ travel_cost_matrix <- function(
 check_time_of_day <- function(time_of_day) {
   if (inherits(time_of_day, "character")) {
     check_string(time_of_day)
-    caller <- rlang::caller_call() 
+    caller <- rlang::caller_call()
     time_of_day <- tryCatch(
-      as.POSIXlt(time_of_day), 
+      as.POSIXlt(time_of_day),
       error = function(e) {
         cli::cli_abort(c("Failed to parse {.arg time_of_day} as a date time object", ">" = e[["message"]]), call = caller)
       }
@@ -117,8 +136,13 @@ is_utc <- function(x, arg = rlang::caller_arg(x)) {
 }
 
 
-validate_u_turns <- function(x, error_arg = rlang::caller_arg(), error_call = rlang::caller_call()) {
-  if (is.null(x)) return(x)
+validate_u_turns <- function(
+    x,
+    error_arg = rlang::caller_arg(),
+    error_call = rlang::caller_call()) {
+  if (is.null(x)) {
+    return(x)
+  }
   x <- rlang::arg_match(
     x,
     values = c("allow_backtrack", "deadend_intersection", "deadend", "no_backtrack"),
@@ -132,3 +156,73 @@ validate_u_turns <- function(x, error_arg = rlang::caller_arg(), error_call = rl
 
   unname(lu[x])
 }
+
+validate_impedance_value <- function(
+    x,
+    error_arg = rlang::caller_arg(x),
+    error_call = rlang::caller_call(),
+    multiple = FALSE
+) {
+  # early return for NULL value
+  if (is.null(x)) {
+    return(x)
+  }
+
+  # known & valid impedance
+  valid_impedance <- c("travel_time", "minutes", "truck_travel_time", "truck_minutes", "walk_time", "miles", "kilometers")
+
+  impedance_param_names <- c("TravelTime", "Minutes", "TruckTravelTime", "TruckMinutes", "WalkTime", "Miles", "Kilometers")
+
+  lu <- setNames(impedance_param_names, valid_impedance)
+
+  impedance <- rlang::arg_match(
+    x,
+    values = valid_impedance,
+    multiple = multiple,
+    error_arg = error_arg,
+    error_call = error_call
+  )
+
+  unname(lu[impedance])
+}
+
+
+validate_restrictions <- function(
+    x,
+    error_arg = rlang::caller_arg(x),
+    error_call = rlang::caller_call()
+) {
+
+  # early return for NULL
+  if (is.null(x)) return(x)
+  
+  # known restriction types
+  restrictions <- c("Any Hazmat Prohibited", "Avoid Carpool Roads", "Avoid Express Lanes", "Avoid Gates", "Avoid Limited Access Roads", "Avoid Private Roads", "Avoid Roads Unsuitable for Pedestrians", "Avoid Stairways", "Avoid Toll Roads", "Avoid Toll Roads for Trucks", "Avoid Truck Restricted Roads", "Avoid Unpaved Roads", "Axle Count Restriction", "Driving a Bus", "Driving a Taxi", "Driving a Truck", "Driving an Automobile", "Driving an Emergency Vehicle", "Height Restriction", "Kingpin to Rear Axle Length Restriction", "Length Restriction", "Preferred for Pedestrians", "Riding a Motorcycle", "Roads Under Construction Prohibited", "Semi or Tractor with One or More Trailers Prohibited", "Single Axle Vehicles Prohibited", "Tandem Axle Vehicles Prohibited", "Through Traffic Prohibited", "Truck with Trailers Restriction", "Use Preferred Hazmat Routes", "Use Preferred Truck Routes", "Walking", "Weight Restriction", "Weight per Axle Restriction", "Width Restriction")
+
+  # set to lowercase for standardized comparision
+  restrictions_lower <- tolower(restrictions)
+
+  # create a lookup vector because we need caps
+  lu <- setNames(restrictions, restrictions_lower)
+  
+  # set to lowercase for check
+  x <- tolower(x)
+
+  # check the provided args
+  restrictions <- rlang::arg_match(
+    x,
+    restrictions_lower,
+    multiple = TRUE,
+    error_arg = error_arg,
+    error_call = error_call
+  )
+
+  # if there is more than one result we need to collapse 
+  if (length(restrictions) > 1) {
+    paste(lu[restrictions], collapse = ",")
+  } else {
+    unname(lu[restrictions])
+  }
+  
+}
+
